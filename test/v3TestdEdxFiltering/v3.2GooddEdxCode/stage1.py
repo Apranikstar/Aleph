eeedf8c9f2255477d5d63513e3de1ee76671f7bc
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-11-01 09:47:54 +0100

    Create stage1.py

---
+++ test/v3TestdEdxFiltering/v3.2GooddEdxCode/stage1.py
@@ -0,0 +1,356 @@
+processList = {
+
+"QQB" : {"fraction" : 0.001},           
+}
+
+
+outputDir = "./output"
+inputDir = "/eos/experiment/aleph/EDM4HEP/MC/1994"
+nCPUS = -1
+includePaths = ["analyzer.h"]
+
+
+class RDFanalysis:
+    def analysers(df):
+
+
+
+        coll = {
+        "GenParticles": "MCParticles",
+        "PFParticles": "RecoParticles",
+        "PFTracks": "EFlowTrack",
+        "PFPhotons": "EFlowPhoton",
+        "PFNeutralHadrons": "EFlowNeutralHadron",
+        "TrackState": "_Tracks_trackStates",
+        "TrackerHits": "TrackerHits",
+        "CalorimeterHits": "CalorimeterHits",
+        "PathLength": "EFlowTrack_L",
+        "Bz": "magFieldBz",
+        }
+
+
+        #### Using Classbit to filter out QQbar samples and then get a specific flavor of jets
+        df = df.Define("jetPID", f"AlephSelection::getJetPID(ClassBitset, {coll['GenParticles']})")
+        df = df.Filter("jetPID == 1")
+        # Define RP kinematics
+        ####################################################################################################
+        df = df.Define("RP_px", "ReconstructedParticle::get_px(RecoParticles)")
+        df = df.Define("RP_py", "ReconstructedParticle::get_py(RecoParticles)")
+        df = df.Define("RP_pz", "ReconstructedParticle::get_pz(RecoParticles)")
+        df = df.Define("RP_e", "ReconstructedParticle::get_e(RecoParticles)")
+        df = df.Define("RP_m", "ReconstructedParticle::get_mass(RecoParticles)")
+
+        # Define pseudo-jets
+        ####################################################################################################
+        df = df.Define("pjetc", "JetClusteringUtils::set_pseudoJets(RP_px, RP_py, RP_pz, RP_e)")
+        # Anti-kt clustering and jet constituents
+        ####################################################################################################
+        df = df.Define("_jet", "JetClustering::clustering_ee_kt(2, 2, 1, 0)(pjetc)")
+        df = df.Define("jets","JetClusteringUtils::get_pseudoJets(_jet)" )
+        df = df.Define("_jetc", "JetClusteringUtils::get_constituents(_jet)") 
+        df = df.Define("jetc", "JetConstituentsUtils::build_constituents_cluster(RecoParticles, _jetc)")
+        df = df.Define("jetConstitutentsTypes", f"AlephSelection::build_constituents_Types(ParticleID, _jetc)")
+
+
+        ############################################# Event Level Variables #######################################################
+        df = df.Define("jet_p4", "JetConstituentsUtils::compute_tlv_jets(jets)" )
+        df = df.Define("event_invariant_mass", "JetConstituentsUtils::InvariantMass(jet_p4[0], jet_p4[1])")
+
+         ########## Picking Jet Flavors
+
+        #df = df.Filter("event_type == 2") # d-quark: 1, u-quark:2, s-quark:3, c-quark:4, b-quark: 5
+
+        # ===== VERTEX
+        # MC primary vertex
+        df = df.Define("pv", f'AlephSelection::get_EventPrimaryVertexP4({coll["GenParticles"]})')
+        ############################################# Particle Flow Level Variables #######################################################
+
+
+        df = df.Define("pfcand_isMu",     "AlephSelection::get_isType(jetConstitutentsTypes,2)")
+        df = df.Define("pfcand_isEl",     "AlephSelection::get_isType(jetConstitutentsTypes,1)")
+        df = df.Define("pfcand_isGamma",  "AlephSelection::get_isType(jetConstitutentsTypes,4)")
+        df = df.Define("pfcand_isChargedHad", "AlephSelection::get_isType(jetConstitutentsTypes,0)")
+        df = df.Define("pfcand_isNeutralHad", "AlephSelection::get_isType(jetConstitutentsTypes,5)")
+
+
+        df = df.Define("jet_nconst", "JetConstituentsUtils::count_consts(jetc)") 
+        df = df.Define(f"jet_nmu",    f"JetConstituentsUtils::count_type(pfcand_isMu)") 
+        df = df.Define(f"jet_nel",    f"JetConstituentsUtils::count_type(pfcand_isEl)") 
+        df = df.Define(f"jet_nchad",  f"JetConstituentsUtils::count_type(pfcand_isChargedHad)") 
+        df = df.Define(f"jet_ngamma", f"JetConstituentsUtils::count_type(pfcand_isGamma)") 
+        df = df.Define(f"jet_nnhad",  f"JetConstituentsUtils::count_type(pfcand_isNeutralHad)")
+        ############################################# Kinematics and PID #######################################################
+
+        df = df.Define("pfcand_e",        "JetConstituentsUtils::get_e(jetc)") 
+        df = df.Define("pfcand_p",        "JetConstituentsUtils::get_p(jetc)") 
+        df = df.Define("pfcand_theta",    "JetConstituentsUtils::get_theta(jetc)") 
+        df = df.Define("pfcand_phi",      "JetConstituentsUtils::get_phi(jetc)") 
+        df = df.Define("pfcand_charge",   "JetConstituentsUtils::get_charge(jetc)") 
+        df = df.Define("pfcand_type",     "JetConstituentsUtils::get_type(jetc)") 
+        df = df.Define("pfcand_erel",     "JetConstituentsUtils::get_erel_cluster(jets, jetc)")
+        df = df.Define("pfcand_erel_log", "JetConstituentsUtils::get_erel_log_cluster(jets, jetc)")
+        df = df.Define("pfcand_thetarel", "JetConstituentsUtils::get_thetarel_cluster(jets, jetc)")
+        df = df.Define("pfcand_phirel",   "JetConstituentsUtils::get_phirel_cluster(jets, jetc)")
+
+        df = df.Define("Bz", '1.5')
+
+
+############################################# Track Parameters and Covariance #######################################################
+
+        df = df.Define("pfcand_dxy",        f'JetConstituentsUtils::XPtoPar_dxy(jetc, {coll["TrackState"]}, pv, Bz)') 
+        df = df.Define("pfcand_dz",         f'JetConstituentsUtils::XPtoPar_dz(jetc, {coll["TrackState"]}, pv, Bz)') 
+        df = df.Define("pfcand_phi0",       f'JetConstituentsUtils::XPtoPar_phi(jetc, {coll["TrackState"]}, pv, Bz)') 
+        df = df.Define("pfcand_C",          f'JetConstituentsUtils::XPtoPar_C(jetc, {coll["TrackState"]}, Bz)') 
+        df = df.Define("pfcand_ct",         f'JetConstituentsUtils::XPtoPar_ct(jetc, {coll["TrackState"]}, Bz)') 
+        df = df.Define("pfcand_dptdpt",     f'JetConstituentsUtils::get_omega_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dxydxy",     f'JetConstituentsUtils::get_d0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dzdz",       f'JetConstituentsUtils::get_z0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dphidphi",   f'JetConstituentsUtils::get_phi0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_detadeta",   f'JetConstituentsUtils::get_tanlambda_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dxydz",      f'JetConstituentsUtils::get_d0_z0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dphidxy",    f'JetConstituentsUtils::get_phi0_d0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_phidz",      f'JetConstituentsUtils::get_phi0_z0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_phictgtheta",f'JetConstituentsUtils::get_tanlambda_phi0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dxyctgtheta",f'JetConstituentsUtils::get_tanlambda_d0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dlambdadz",  f'JetConstituentsUtils::get_tanlambda_z0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_cctgtheta",  f'JetConstituentsUtils::get_omega_tanlambda_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_phic",       f'JetConstituentsUtils::get_omega_phi0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_dxyc",       f'JetConstituentsUtils::get_omega_d0_cov(jetc, {coll["TrackState"]})') 
+        df = df.Define("pfcand_cdz",        f'JetConstituentsUtils::get_omega_z0_cov(jetc, {coll["TrackState"]})')
+
+
+
+############################################# Btag Variables #######################################################
+
+        df = df.Define("pfcand_btagSip2dVal",   "JetConstituentsUtils::get_Sip2dVal_clusterV(jets, pfcand_dxy, pfcand_phi0, Bz)") 
+        df = df.Define("pfcand_btagSip2dSig",   "JetConstituentsUtils::get_Sip2dSig(pfcand_btagSip2dVal, pfcand_dxydxy)") 
+        df = df.Define("pfcand_btagSip3dVal",   "JetConstituentsUtils::get_Sip3dVal_clusterV(jets, pfcand_dxy, pfcand_dz, pfcand_phi0, Bz)") 
+        df = df.Define("pfcand_btagSip3dSig",   "JetConstituentsUtils::get_Sip3dSig(pfcand_btagSip3dVal, pfcand_dxydxy, pfcand_dzdz)") 
+        df = df.Define("pfcand_btagJetDistVal","JetConstituentsUtils::get_JetDistVal_clusterV(jets, jetc, pfcand_dxy, pfcand_dz, pfcand_phi0, Bz)") 
+        df = df.Define("pfcand_btagJetDistSig","JetConstituentsUtils::get_JetDistSig(pfcand_btagJetDistVal, pfcand_dxydxy, pfcand_dzdz)")
+
+        ############################################# Jet Level Variables #######################################################
+        df=df.Define("event_njet",   "JetConstituentsUtils::count_jets(jetc)")
+        df = df.Filter("event_njet > 1")
+
+
+        # compute jet observables
+        df = df.Define("jet_p", "JetClusteringUtils::get_p(jets)")
+        df = df.Define("jet_px", "JetClusteringUtils::get_px(jets)")
+        df = df.Define("jet_py", "JetClusteringUtils::get_py(jets)")
+        df = df.Define("jet_pz", "JetClusteringUtils::get_pz(jets)")
+        df = df.Define("jet_pT", "JetClusteringUtils::get_pt(jets)")
+
+        df = df.Define("jet_e", "JetClusteringUtils::get_e(jets)")
+        df = df.Define("jet_mass", "JetClusteringUtils::get_m(jets)")
+        df = df.Define("jet_phi", "JetClusteringUtils::get_phi(jets)")
+        df = df.Define("jet_theta", "JetClusteringUtils::get_theta(jets)")
+        df = df.Define("jet_eta", "JetClusteringUtils::get_eta(jets)")
+
+        ##############################################################################################################
+
+
+        df = df.Define("pfcand_dEdx_pads_objs", "AlephSelection::build_constituents_dEdx()(RecoParticles, _RecoParticles_tracks.index, dEdxPads, _dEdxPads_track.index, _jetc)" )
+        df = df.Define("pfcand_dEdx_pads_type", "AlephSelection::get_dEdx_type(pfcand_dEdx_pads_objs)")
+        df = df.Define("pfcand_dEdx_pads_value", "AlephSelection::get_dEdx_value(pfcand_dEdx_pads_objs)")
+        df = df.Define("pfcand_dEdx_pads_error", "AlephSelection::get_dEdx_error(pfcand_dEdx_pads_objs)")
+
+        df = df.Define("pfcand_dEdx_wires_objs", "AlephSelection::build_constituents_dEdx()(RecoParticles, _RecoParticles_tracks.index, dEdxWires, _dEdxWires_track.index, _jetc)" )
+        df = df.Define("pfcand_dEdx_wires_type", "AlephSelection::get_dEdx_type(pfcand_dEdx_wires_objs)")
+        df = df.Define("pfcand_dEdx_wires_value", "AlephSelection::get_dEdx_value(pfcand_dEdx_wires_objs)")
+        df = df.Define("pfcand_dEdx_wires_error", "AlephSelection::get_dEdx_error(pfcand_dEdx_wires_objs)")
+
+
+
+## ELECTRONS — Pads
+
+        df = df.Define("pfcand_dEdx_pads_el",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxPads, _dEdxPads_track.index,"
+               "_jetc, 1)")
+        df = df.Define("pfcand_dEdx_el_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_pads_el)")
+        df = df.Define("pfcand_dEdx_el_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_pads_el)")
+
+
+## ELECTRONS — Wires
+
+        df = df.Define("pfcand_dEdx_wires_el",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxWires, _dEdxWires_track.index,"
+               "_jetc, 1)")
+        df = df.Define("pfcand_dEdx_el_wire_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_wires_el)")
+        df = df.Define("pfcand_dEdx_el_wire_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_wires_el)")
+
+
+
+## MUONS — Pads
+
+        df = df.Define("pfcand_dEdx_pads_mu",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxPads, _dEdxPads_track.index,"
+               "_jetc, 2)")
+        df = df.Define("pfcand_dEdx_mu_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_pads_mu)")
+        df = df.Define("pfcand_dEdx_mu_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_pads_mu)")
+
+
+# MUONS — Wires
+
+        df = df.Define("pfcand_dEdx_wires_mu",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxWires, _dEdxWires_track.index,"
+               "_jetc, 2)")
+        df = df.Define("pfcand_dEdx_mu_wire_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_wires_mu)")
+        df = df.Define("pfcand_dEdx_mu_wire_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_wires_mu)")
+
+
+
+# CHARGED HADRONS — Pads (π/K/p)
+
+        df = df.Define("pfcand_dEdx_pads_ch",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxPads, _dEdxPads_track.index,"
+               "_jetc, 0)")
+        df = df.Define("pfcand_dEdx_ch_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_pads_ch)");
+        df = df.Define("pfcand_dEdx_ch_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_pads_ch)");
+
+
+# CHARGED HADRONS — Wires
+
+        df = df.Define("pfcand_dEdx_wires_ch",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxWires, _dEdxWires_track.index,"
+               "_jetc, 0)")
+        df = df.Define("pfcand_dEdx_ch_wire_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_wires_ch)")
+        df = df.Define("pfcand_dEdx_ch_wire_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_wires_ch)")
+
+
+
+# PHOTONS — Pads
+
+        df = df.Define("pfcand_dEdx_pads_ph",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxPads, _dEdxPads_track.index,"
+               "_jetc, 4)")
+        df = df.Define("pfcand_dEdx_ph_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_pads_ph)")
+        df = df.Define("pfcand_dEdx_ph_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_pads_ph)")
+
+
+## PHOTONS — Wires
+
+        df = df.Define("pfcand_dEdx_wires_ph",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxWires, _dEdxWires_track.index,"
+               "_jetc, 4)")
+        df = df.Define("pfcand_dEdx_ph_wire_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_wires_ph)")
+        df = df.Define("pfcand_dEdx_ph_wire_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_wires_ph)")
+
+
+
+# NEUTRAL HADRONS — Pads
+
+        df = df.Define("pfcand_dEdx_pads_nh",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxPads, _dEdxPads_track.index,"
+               "_jetc, 5)")
+        df = df.Define("pfcand_dEdx_nh_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_pads_nh)")
+        df = df.Define("pfcand_dEdx_nh_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_pads_nh)")
+
+
+# NEUTRAL HADRONS — Wires
+
+        df = df.Define("pfcand_dEdx_wires_nh",
+               "AlephSelection::build_constituents_dEdx_filtered()("
+               "jetConstitutentsTypes,"
+               "RecoParticles, _RecoParticles_tracks.index,"
+               "dEdxWires, _dEdxWires_track.index,"
+               "_jetc, 5)")
+        df = df.Define("pfcand_dEdx_nh_wire_value_good",
+               "AlephSelection::get_good_dEdx_value(pfcand_dEdx_wires_nh)")
+        df = df.Define("pfcand_dEdx_nh_wire_error_good",
+               "AlephSelection::get_good_dEdx_error(pfcand_dEdx_wires_nh)")
+
+
+        return df
+
+    def output():
+
+        return [
+         #  "pfcand_dEdx_el_type", "pfcand_dEdx_el_value", "pfcand_dEdx_el_error",
+            ### Event Level Variables
+
+
+                # Pads
+    "pfcand_dEdx_el_type", "pfcand_dEdx_el_value", "pfcand_dEdx_el_error",
+    "pfcand_dEdx_mu_type", "pfcand_dEdx_mu_value", "pfcand_dEdx_mu_error",
+    "pfcand_dEdx_ch_type", "pfcand_dEdx_ch_value", "pfcand_dEdx_ch_error",
+    # Wires
+    "pfcand_dEdx_el_wire_type", "pfcand_dEdx_el_wire_value", "pfcand_dEdx_el_wire_error",
+    "pfcand_dEdx_mu_wire_type", "pfcand_dEdx_mu_wire_value", "pfcand_dEdx_mu_wire_error",
+    "pfcand_dEdx_ch_wire_type", "pfcand_dEdx_ch_wire_value", "pfcand_dEdx_ch_wire_error",
+
+
+            "event_invariant_mass","event_njet",  
+
+            # Jet Level Variables
+            "jet_mass","jet_e",
+            "jet_phi", "jet_theta", "jet_eta", 
+            "jet_p", "jet_px", "jet_py", "jet_pz", "jet_pT",
+
+            "jet_nconst",
+            "jet_nnhad","jet_nchad",
+            "jet_nel", "jet_nmu", 
+            "jet_ngamma",
+
+            #the dEdX values associated to the jet constituents:
+            "pfcand_dEdx_pads_type", "pfcand_dEdx_pads_value", "pfcand_dEdx_pads_error",
+            "pfcand_dEdx_wires_type", "pfcand_dEdx_wires_value", "pfcand_dEdx_wires_error",
+               
+                
+            "pfcand_isMu", "pfcand_isEl", "pfcand_isChargedHad", "pfcand_isGamma", "pfcand_isNeutralHad",
+            "pfcand_e", "pfcand_p", "pfcand_theta", "pfcand_phi", "pfcand_charge", "pfcand_type",
+            "pfcand_erel", "pfcand_erel_log", "pfcand_thetarel", "pfcand_phirel", 
+
+            "pfcand_dxy", "pfcand_dz", "pfcand_phi0", "pfcand_C", "pfcand_ct",
+            "pfcand_dptdpt", "pfcand_dxydxy", "pfcand_dzdz", "pfcand_dphidphi", "pfcand_detadeta",
+            "pfcand_dxydz", "pfcand_dphidxy", "pfcand_phidz", "pfcand_phictgtheta", "pfcand_dxyctgtheta",
+            "pfcand_dlambdadz", "pfcand_cctgtheta", "pfcand_phic", "pfcand_dxyc", "pfcand_cdz",
+ 
+            "pfcand_btagSip2dVal", "pfcand_btagSip2dSig", "pfcand_btagSip3dVal", "pfcand_btagSip3dSig", 
+            "pfcand_btagJetDistVal", "pfcand_btagJetDistSig",
+                ]
